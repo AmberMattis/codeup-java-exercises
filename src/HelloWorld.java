cfe23052305bd0663862946a51e6b4cb1908c27f
Amber Mattis <amber.mattis2@gmail.com>
2020-12-02 10:30:18 -0600

    syntax practice

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,5 +2,19 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println();
+
+        int myFavoriteNumber = 75;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I'm confused";
+        System.out.println(myString);
+
+        // myString = 3.24259;
+        // compiler error incompadable typed double can't be converted to a string
+
+        //long myNumber;
+        //System.out.println(myNumber);
+        //compiler error variable myNumber might not have been initialized
+
     }
 }
