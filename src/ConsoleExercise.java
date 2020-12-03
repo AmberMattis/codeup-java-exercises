1da0e141ae79a16db14f7c4d9b0b3181e7562470
Amber Mattis <amber.mattis2@gmail.com>
2020-12-02 21:10:15 -0600

    exercise 1 comp

---
+++ src/ConsoleExercise.java
@@ -0,0 +1,10 @@
+
+public class ConsoleExercise {
+    public static void main(String[] arg) {
+
+        //1//
+
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f.%n", pi);
+    }
+}
