4c1f55491815143a579aaf078c6047491f926e10
Amber Mattis <amber.mattis2@gmail.com>
2020-12-13 17:43:50 -0600

    arrays exercise

--- src/ArraysExercise.java
+++ src/ArraysExercise.java
@@ -1,33 +1,44 @@
 import java.util.Arrays;
 public class ArraysExercise {
 
-    public static Person addPerson(Person person){
-        return person;
 
+    public static Person[] addPerson(Person[] pplArray, Person personToAdd){
+        Person[] tempArray = Arrays.copyOf(pplArray, pplArray.length+1);
+        tempArray[tempArray.length-1] = personToAdd;
+        return tempArray;
     }
 
 
     public static void main(String[] args){
         int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(numbers);// souting this out will only show the unique id for the array not the values
+        //System.out.println(numbers);// souting this out will only show the unique id for the array not the values
         System.out.println(Arrays.toString(numbers));
 
-        Person[] person = new Person[3];
+        Person[] people = new Person[3];
 
-        Person p1 = new Person("Amber");
-        Person p2 = new Person("Andrea");
-        Person p3 = new Person("Ashley");
+        people[0] = new Person("Amber");
+        people[1] = new Person("Andrea");
+        people[2] = new Person("Ashley");
 
-        person[0] = p1;
-        person[1] = p2;
-        person[2] = p3;
 
-        System.out.println(Arrays.toString(person));
-        for (int i = 0; i < person.length; i += 1) {
-            System.out.println(person[i].getName());
-       }
+//        System.out.println(Arrays.toString(people));
+//        for (int i = 0; i < people.length; i += 1) {
+//            System.out.println(people[i].getName());
+//       }
 
-        System.out.println(addPerson(p1));
+
+        //This enhanced for loop takes in each people object in the array and
+        for(Person person : people){
+            System.out.println(person.getName());
+        }
+
+        Person[] newArray = addPerson(people, new Person("April"));
+
+        for(Person person: newArray){
+            System.out.println(person.getName());
+        }
+
+
 
     }
 }
