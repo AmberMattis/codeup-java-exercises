c899d812b471871493a5eb19673e88f88541468a
Amber Mattis <amber.mattis2@gmail.com>
2020-12-18 10:53:52 -0600

    Exceptions lecture

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -5,29 +5,40 @@ import java.util.Scanner;
 public class MoviesApplication {
 
     public static void main(String[] args){
-        System.out.println("What would you like to do?\n");
-        System.out.println("0 - exit");
-        System.out.println("1 - view all movies");
-        System.out.println("2 - view movies in the animated category");
-        System.out.println("view movies in the drama category");
-        System.out.println("view movies in the horror category");
-        System.out.println("view movies in the scifi category\n");
-
-        System.out.print("Enter your choice: ");
-        String first = "1";
-        Input userInput = new Input();
-        Scanner scn = new Scanner(System.in);
-        String userResponse = scn.next();
-
-//        if(userResponse.equals(first)) {
-//            for(MoviesArray Movie[]: movies){
-//
-//            }
-//
-
-
-            System.out.println(userResponse);
+       // userOptions();
 
         }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        public static void userOptions() {
+//            System.out.println("What would you like to do?\n");
+//            System.out.println("0 - exit");
+//            System.out.println("1 - view all movies");
+//            System.out.println("2 - view movies in the animated category");
+//            System.out.println("3 - view movies in the drama category");
+//            System.out.println("4 - view movies in the horror category");
+//            System.out.println("5 - view movies in the scifi category\n");
+//            return output;
+//        }
+//
+//        System.out.print("Enter your choice: ");
+//        Scanner scn = new Scanner(System.in);
+//         String userResponse = scn.next();
+//        System.out.println(userResponse);
+
+}
 
